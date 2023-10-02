d55da45d92b1450989d54c1dc28abce16cb1e08d
Emeka Okezie <eokezie@nasdng.com>
2023-10-02 14:22:42 +0100

    feat: Created the Hero Component

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <section className='px-6 md:px-20 py-24'>
-        <div className='flex max-xl:flex-col gap-16'>
+        <div className='flex max-xl:flex-col gap-16 items-center'>
           <div className='flex flex-col jusify-center'>
             <p className='small-text'>
               Smart Shopping Starts Here:
